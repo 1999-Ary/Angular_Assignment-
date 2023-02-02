acb41abd4fa17280d0069b921a282cf7e60abd3b
Aryan Singh Jyoti <92636439+1999-Ary@users.noreply.github.com>
2023-02-02 11:36:40 +0530

    added files

---
+++ angular db.sql
@@ -0,0 +1,18 @@
+create table mydb4(
+	empcode integer,
+	name varchar(20),
+	designation varchar(30),
+	email varchar(30),
+	location varchar(30));
+
+Insert into mydb4 values(1,'aryan','manager','aryan@gmail.com','pune'),
+(2,'harsh','hr','harsh@gmail.com','pune'),
+(3,'nishant','Developer','nishant@gmail.com','mumbai')
+	
+	
+	
+	
+	
+select *from mydb4;
+
+
